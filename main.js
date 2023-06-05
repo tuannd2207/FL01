8e2678452a64070a770fecf25f13628c003cb874
tuannd <tuannd2207@gmail.com>
2023-06-05 23:42:03 +0700

    index

--- main.js
+++ main.js
@@ -14,3 +14,48 @@ owl.owlCarousel({
     autoplayTimeout:1000,
     autoplayHoverPause:true
 });
+gsap.registerPlugin(ScrollTrigger);
+// gsap.from(".pre-index-content-title", {
+//     scrollTrigger: ".pre-index-content-title", // same as scrollTrigger: {trigger: ".line-1", toggleActions: "play none none none", start: "top bottom", end: "bottom top"}
+//     fontSize: 0,
+//     duration: 3,
+//     transformOrigin: "left center",
+//     toggleActions:"restart complete reverse reset"
+//     easing\
+// });
+gsap.from(".pre-index-content-title", {
+    duration:2, opacity:0, ease:"linear",
+    scrollTrigger: {
+        trigger:".pre-index-content-title",
+        start:"top 75%", //when top of herman passes 75% viewport height
+        end:"bottom 25%", //when bottom of herman passes 25% viewport height
+
+        //events: onEnter onLeave onEnterBack onLeaveBack
+        toggleActions:"restart complete resume reset"
+        //options: play, pause, resume, reset, restart, complete, reverse,none
+    }
+});
+gsap.from(".pre-img", {
+    duration:2, opacity:0, ease:"linear",
+    scrollTrigger: {
+        trigger:".pre-img",
+        start:"top 75%", //when top of herman passes 75% viewport height
+        end:"bottom 25%", //when bottom of herman passes 25% viewport height
+
+        //events: onEnter onLeave onEnterBack onLeaveBack
+        toggleActions:"restart complete resume reset"
+        //options: play, pause, resume, reset, restart, complete, reverse,none
+    }
+})
+gsap.from(".border-deco", {
+    duration:1, scaleY:0, ease:"linear",
+    scrollTrigger: {
+        trigger:".border-deco",
+        start:"top 75%", //when top of herman passes 75% viewport height
+        end:"bottom 25%", //when bottom of herman passes 25% viewport height
+
+        //events: onEnter onLeave onEnterBack onLeaveBack
+        toggleActions:"restart complete resume reset"
+        //options: play, pause, resume, reset, restart, complete, reverse,none
+    }
+})
